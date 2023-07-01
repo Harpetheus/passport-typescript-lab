5bffbe2203f7ec4fa144f09f89445daf5502e843
Harpetheus <101912024+Harpetheus@users.noreply.github.com>
2023-06-30 21:10:11 -0700

    Stage 4

--- routes/adminRoute.ts
+++ routes/adminRoute.ts
@@ -5,9 +5,20 @@ import { forwardAuthenticated } from "../middleware/checkAuth";
 import session from "express-session";
 
 router.get("/", ensureAuthenticated, (req, res) => {
+  const sessionID = req.sessionID;
+  const sessionStore = req.sessionStore;
+  sessionStore.get(sessionID, (err, session)=>{
+    if(err){
+    console.log(err)
+    }else{
+       res.json(session);
+    }
+  })
   res.render("admin", {
     user: req.user,
+    session
   });
+
 });
 
 export default router;
